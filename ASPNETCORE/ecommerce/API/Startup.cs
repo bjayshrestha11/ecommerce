810b38408295ed5ea6f23ec711777fa7c090863e
aistha11 <bijayshrestha0908@gmail.com>
2020-10-13 15:50:32 +0545

    Pagination Sort Order & Cors

--- ASPNETCORE/ecommerce/API/Startup.cs
+++ ASPNETCORE/ecommerce/API/Startup.cs
@@ -29,6 +29,11 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
